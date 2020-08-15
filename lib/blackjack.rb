f0e65da6bc20e319b87568cebc46a4b32ac281ce
Jasper Reed <jmixon.reed@gmail.com>
2020-08-15 01:38:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -72,9 +72,9 @@ def runner
   welcome
   card_total = initial_round
   
-  while card_total <= 21 do
+  while card_total < 21 do
     card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
+    display_card_total(card_total)
   end
   end_game(card_total)
 end
